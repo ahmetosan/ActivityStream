2dbf8385728431faaec8487ac0c0eb3c4e72be46
hasan <hasanmamedoff@gmail.com>
2020-04-14 05:34:31 -0400

    User story number 7 test case completed

--- src/test/java/com/automation/activitystream/pages/ActivityStreamPage.java
+++ src/test/java/com/automation/activitystream/pages/ActivityStreamPage.java
@@ -1,11 +1,14 @@
 package com.automation.activitystream.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import utilities.BrowserUtils;
 
 public class ActivityStreamPage extends PageBase{
+
+
 
     @FindBy(css = "[id='feed-add-post-form-link-text']")
     private WebElement moreButton;
@@ -17,7 +20,7 @@ public class ActivityStreamPage extends PageBase{
     private WebElement announcementIframe;
 
     @FindBy(xpath = "//body[@contenteditable='true']")
-    private WebElement AnnouncementTextBox;
+    private WebElement announcementTextBox;
 
     private String sendingText = "Announcement was sent";
 
@@ -27,6 +30,13 @@ public class ActivityStreamPage extends PageBase{
     @FindBy(xpath = "//div[@class='feed-post-text-block feed-info-block']//div[text()='xvxvc']")
     private WebElement AnnouncementMessage;
 
+public String getAnnounceMessage(){
+    String result = enteringText();
+    String messageXpath = "//div[@class='feed-post-text-block feed-info-block']//div[text()='"+result+"']";
+    WebElement msg = driver.findElement(By.xpath(messageXpath));
+
+    return msg.getText();
+}
 
 
     public void clickMoreButton(){
@@ -42,6 +52,28 @@ public class ActivityStreamPage extends PageBase{
         BrowserUtils.wait(1);
         announcement.click();
 
+    }
+
+    public String enteringText(){
+        String sendWord = "Sended By Hasan";
+        return sendWord;
+    }
+
+    public void sendTextAnnouncement(){
+        driver.switchTo().frame(announcementIframe);
+        BrowserUtils.waitForPageToLoad(12);
+
+        announcementTextBox.sendKeys(enteringText());
+        BrowserUtils.waitForPageToLoad(12);
+
+        driver.switchTo().defaultContent();
+        BrowserUtils.waitForPageToLoad(12);
+
+    }
+
+    public void clickSendBtnAnnouncement(){
+        sendButton.click();
+        BrowserUtils.wait(2);
     }
 
 
--- src/test/java/com/automation/activitystream/tests/ActivityStreamTest.java
+++ src/test/java/com/automation/activitystream/tests/ActivityStreamTest.java
@@ -2,17 +2,34 @@ package com.automation.activitystream.tests;
 
 import com.automation.activitystream.pages.ActivityStreamPage;
 import com.automation.activitystream.pages.LoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ActivityStreamTest extends TestBase{
 
     @Test
     public void ClickMoreUnderActivityStreamAnnouncement(){
+        test = report.createTest("send announcement from Announcement Activity Stream.");
+
         LoginPage loginPage = new LoginPage();
         loginPage.login();
+        test.info("Login as a helpdesk47");
+
+
         ActivityStreamPage activityStreamPage = new ActivityStreamPage();
         activityStreamPage.clickMoreButton();
         activityStreamPage.clickAnnouncement();
+        activityStreamPage.sendTextAnnouncement();
+        activityStreamPage.clickSendBtnAnnouncement();
+        String actual = activityStreamPage.getAnnounceMessage();
+        actual=actual.substring(0,actual.indexOf("\n"));
+
+        Assert.assertEquals(actual,activityStreamPage.enteringText());
+        test.pass("User successfully sent announcement from Announcement subheading from \"More\" under Activity Stream. ");
+
+
+
+
 
 
     }
