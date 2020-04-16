16045abc0139180e4c491d02742b736e2051d800
ayamilo <ayamilo2000@gmail.com>
2020-04-16 17:54:17 -0400

    Mohammed test on Q1

--- src/test/java/com/automation/activitystream/pages/MessageLinkPage_1.java
+++ src/test/java/com/automation/activitystream/pages/MessageLinkPage_1.java
@@ -9,9 +9,9 @@ import utilities.Driver;
 
 public  class MessageLinkPage_1 extends PageBase {
 
-public MessageLinkPage_1(){
-    PageFactory.initElements(Driver.getDriver(),this);
-}
+    public MessageLinkPage_1() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 
     @FindBy(xpath = "//span[@id='feed-add-post-form-tab-message']")
@@ -22,40 +22,50 @@ public MessageLinkPage_1(){
     private WebElement urlLink;
     @FindBy(css = ".adm-btn-save")
     private WebElement saveBtn;
+    @FindBy(xpath = "/html/body/a")
+    private WebElement savedText;
+    @FindBy(xpath = "//button[@id='blog-submit-button-save']")
+    private WebElement sendText;
 
-
-    public void goToMessage(){
+    public void goToMessage() {
         BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.visibilityOf(MessageClick));
         MessageClick.click();
 
     }
 
-    public void clickLink(){
+    public void clickLink() {
         BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.visibilityOf(Link));
         Link.click();
 
     }
 
-    public void textInputUrlLink (){
+    public void textInputUrlLink() {
         BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.visibilityOf(urlLink));
         urlLink.sendKeys("http://bcc.com");
 
     }
 
-    public void  clickSave  (){
+    public void clickSave() {
         BrowserUtils.waitForPageToLoad(20);
         wait.until(ExpectedConditions.visibilityOf(saveBtn));
-       saveBtn.click();
+        saveBtn.click();
 
     }
 
 
+    public void savedText() {
+        BrowserUtils.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(savedText));
+        savedText.isDisplayed();
 
+    }
 
-
-
-
+    public void sendText() {
+        BrowserUtils.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(sendText));
+        sendText.click();
+    }
 }
--- src/test/java/com/automation/activitystream/tests/MessageLinkTest_1.java
+++ src/test/java/com/automation/activitystream/tests/MessageLinkTest_1.java
@@ -2,14 +2,16 @@ package com.automation.activitystream.tests;
 
 import com.automation.activitystream.pages.LoginPage;
 import com.automation.activitystream.pages.MessageLinkPage_1;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MessageLinkTest_1 extends TestBase{
 
 
     @Test
-    public  void MohTest(){
-        test = report.createTest("More appreciation Test");
+    public  void MohammedTest(){
+        test = report.createTest("Message Link Test");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
 
@@ -18,8 +20,10 @@ public class MessageLinkTest_1 extends TestBase{
         mohammed1.clickLink();
         mohammed1.textInputUrlLink();
         mohammed1.clickSave();
+        mohammed1.sendText();
+        mohammed1.savedText();
 
-
+        //String expected ="http//bbc.com";
 
 
 
